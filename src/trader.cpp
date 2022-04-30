7b24a66fbb3a306a10cc96730df6902e5acc6115
chenqinxing <565763914@qq.com>
2022-05-01 01:59:24 +0800

    add trader

---
+++ src/trader.cpp
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include "ThostFtdcMdApi.h"
+#include "ctp_market.hpp"
+#include "log.hpp"
+#include <nanomsg/nn.hpp>
+#include <nanomsg/pubsub.h>
+#include <time.h>
+#include "pubsub/pub.hpp"
+#include "pubsub/sub.hpp"
+#include "ctp_trade.hpp"
+#include "tools/conv.hpp"
+#include "symbol_holder.hpp"
+
+
+int main() {
+    
+}
